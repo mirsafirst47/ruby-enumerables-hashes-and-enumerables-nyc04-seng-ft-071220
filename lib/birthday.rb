d1f5457b62b301c08219a447b19e4e2b3b3eb2ad
Samir Triande <habib.triande@gmail.com>
2020-07-21 12:24:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,6 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each { |key, value| puts "Happy Birthday #{key}! You are now #{value} years old!" }
+  birthday_kids.each { |kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!" }
   #end
 end
